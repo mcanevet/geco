8799a0ba51cb0022c722a28d6cbffd8928923f73
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-29 10:03:27 +0200

    feat(core): support verbose mode

--- geco.py
+++ geco.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import argparse
+import logging
+import sys
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    return parser
+
+
 def main():
     pass
 
 
 if __name__ == "__main__":
+    args = create_parser().parse_args(sys.argv[1:])
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("verbosity turned on")
     main()
--- tests/test_geco.py
+++ tests/test_geco.py
@@ -1,5 +1,7 @@
-from geco import main
+from geco import main, create_parser
 import pytest
+import unittest
+from argparse import Namespace
 
 
 def test_init_success():
@@ -9,3 +11,20 @@ def test_init_success():
 def test_init_failure():
     with pytest.raises(TypeError):
         main("foo")
+
+
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = create_parser()
+
+    def test_parse_args_without_args(self):
+        args = self.parser.parse_args()
+        assert(args == Namespace(verbose=False))
+
+    def test_parse_args_with_verbose_short(self):
+        args = self.parser.parse_args(["-v"])
+        assert(args == Namespace(verbose=True))
+
+    def test_parse_args_with_verbose_long(self):
+        args = self.parser.parse_args(["--verbose"])
+        assert(args == Namespace(verbose=True))
