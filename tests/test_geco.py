e9dd517e8eb655e1d513da8228cadf773c873802
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-29 13:22:01 +0200

    feat(core): add mandatory profile argument

--- tests/test_geco.py
+++ tests/test_geco.py
@@ -1,4 +1,5 @@
 from geco import main, create_parser
+import pathlib
 import pytest
 import unittest
 from argparse import Namespace
@@ -18,13 +19,26 @@ class ParserTest(unittest.TestCase):
         self.parser = create_parser()
 
     def test_parse_args_without_args(self):
-        args = self.parser.parse_args()
-        assert(args == Namespace(verbose=False))
+        with pytest.raises(SystemExit):
+            self.parser.parse_args()
 
     def test_parse_args_with_verbose_short(self):
-        args = self.parser.parse_args(["-v"])
-        assert(args == Namespace(verbose=True))
+        args = self.parser.parse_args(["-v", "-p", "examples/x230"])
+        assert(args == Namespace(verbose=True,
+                                 profile=pathlib.PosixPath("examples/x230")))
 
     def test_parse_args_with_verbose_long(self):
-        args = self.parser.parse_args(["--verbose"])
-        assert(args == Namespace(verbose=True))
+        args = self.parser.parse_args(["--verbose",
+                                       "--profile", "examples/x230"])
+        assert(args == Namespace(verbose=True,
+                                 profile=pathlib.PosixPath("examples/x230")))
+
+    def test_parse_args_with_profile_short(self):
+        args = self.parser.parse_args(["-p", "examples/x230"])
+        assert(args == Namespace(verbose=False,
+                                 profile=pathlib.PosixPath("examples/x230")))
+
+    def test_parse_args_with_profile_long(self):
+        args = self.parser.parse_args(["--profile", "examples/x230"])
+        assert(args == Namespace(verbose=False,
+                                 profile=pathlib.PosixPath("examples/x230")))
