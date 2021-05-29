f8c6c2befd3b1860ea277081edfe5f4664a9c3c2
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-29 08:16:09 +0200

    test: test when passing arg to main()

--- tests/test_geco.py
+++ tests/test_geco.py
@@ -1,5 +1,11 @@
 from geco import main
+import pytest
 
 
-def test_init():
+def test_init_success():
     main()
+
+
+def test_init_failure():
+    with pytest.raises(TypeError):
+        main("foo")
